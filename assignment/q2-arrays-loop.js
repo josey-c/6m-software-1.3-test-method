13c72644f70955af418e4f6f91b28f1179ff613a
josey-c <josephchng95@gmail.com>
2023-05-29 22:34:38 +0800

    done

--- assignment/q2-arrays-loop.js
+++ assignment/q2-arrays-loop.js
@@ -7,7 +7,9 @@
 const studentList = ["Johnson", "Christina", "Michael", "Lina", "Larry", "Michelle", "Bob", "Wilson"]
 
 function printStudentNames(){
-    // Add code here
+    for (let i in studentList) {
+        console.log(studentList[i]);
+    }
 }
 
 printStudentNames();
